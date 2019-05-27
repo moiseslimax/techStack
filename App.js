5e917461e942ff73c09f14aeae569c8b0d0eecd4
Moisés Maldaner de Lima <moises.lima@meta.com.br>
2019-05-27 12:32:57 -0300

    redux boilerplate

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './src/reducers'
+
+const App = () => {
+  return (
+    <Provider store={createStore(reducers)}>
+      <View><Text>teste de string</Text></View>
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
