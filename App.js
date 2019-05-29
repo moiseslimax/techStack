d8cac4e728c7eb1a227d977e4ee8da46b63287da
Moisés Maldaner de Lima <moiseslimax@gmail.com>
2019-05-28 21:03:31 -0300

    Some changes

--- App.js
+++ App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import reducers from './src/reducers'
+import reducers from './src/reducers';
+import Header from './src/components/common/Header';
 
 const App = () => {
   return (
-    <Provider store={createStore(reducers)}>
-      <View><Text>teste de string</Text></View>
+    <Provider store={createStore(reducers)}> 
+      <View>
+        <Header headerText="Tech Stack" />
+        <Text>teste de string</Text>
+      </View>
     </Provider>
   );
 }
---
+++ src/components/LibraryList.js
@@ -0,0 +1,10 @@
+import React,  { Component } from 'react';
+
+
+class LibraryList extends Component {
+    render() {
+        return;
+    }
+}
+
+export default LibraryList;
