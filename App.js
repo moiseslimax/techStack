0928ad5d257cc8650d673f5035838bba082559c6
Moisés Maldaner de Lima <moiseslimax@gmail.com>
2019-06-03 22:18:50 -0300

    Redux data with connect

--- App.js
+++ App.js
@@ -4,13 +4,15 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './src/reducers';
 import Header from './src/components/common/Header';
+import LibraryList from './src/components/LibraryList';
+
 
 const App = () => {
   return (
     <Provider store={createStore(reducers)}> 
       <View>
         <Header headerText="Tech Stack" />
-        <Text>teste de string</Text>
+        <LibraryList></LibraryList>
       </View>
     </Provider>
   );
